60a79c11f51e4b0deae49a45dcb6638515f2f821
kino <87764343+kino-brex@users.noreply.github.com>
2021-08-04 17:27:33 +0900

    메인 > 헤더 > 알람 / jquery 추가

--- src/components/component-name/Alram.vue
+++ src/components/component-name/Alram.vue
@@ -1,40 +1,85 @@
 <template>
-  <div class="modal-popup">
-    <v-btn @click="$emit('close')" class="btn-close">Close
-      <span class="a11y">Close</span>
-    </v-btn>	
+	<div class="modal-popup alram_page">
+		<div class="modal-head">
+				<v-btn @click="closeModal()" class="modal_btn-close">
+				<div>
+					<span class="line01"></span>
+					<span class="line02"></span>
+					<span class="a11y">Close</span>
+				</div>
+			</v-btn>	
+		</div>
+		<div class="modal-inner">
+			<h3>알림</h3>   
 
-    Alram
-  </div>
+			<div class="list_wrap">
+				<v-card class="list_box">
+					<v-list-item class="_item">
+						<span class="ic-36 alram-type01">
+							<i><span class="a11y">new</span></i>
+						</span>
+						<a>
+							<p>이소현 한국에서 메이크업 아티스트로 활동하실 계획이시라면 대학교 활동하실 계획이시라면 대학교</p>
+							<span>등록한 답변이 <span class="colr_yellow">채택</span> 되었어요.</span>
+						</a>
+						<span class="time">방금 전</span>
+					</v-list-item>
+
+					<v-list-item class="_item">
+						<span class="ic-36 alram-type02">
+							<i><span class="a11y">new</span></i>
+						</span>
+						<a>
+							<p>타입별 기초 제품 고르는 법 라이브 방송이 곧 시작 됩니다.</p>
+							<span><span class="colr_green">라이브</span> 방송 10분 전</span>
+						</a>
+						<span class="time">10분 전</span>
+					</v-list-item>
+				</v-card>
+
+				<div class="pass_break">
+					이전 알림
+				</div>
+				<v-card class="list_box">
+					<v-list-item class="_item">
+						<span class="ic-36 alram-type01">
+							<!-- <i><span class="a11y">new</span></i> -->
+						</span>
+						<a>
+							<p>알림 안내 문구를 노출합니다. 알림 안내 문구를 노출합니다.</p>
+							<span><i class="ic-24 ic_attach"></i> 링크 관련 안내문구</span>
+						</a>
+						<span class="time">방금 전</span>
+					</v-list-item>
+
+					<v-list-item class="_item">
+						<span class="ic-36 alram-type02">
+							<!-- <i><span class="a11y">new</span></i> -->
+						</span>
+						<a>
+							<p>타입별 기초 제품 고르는 법 라이브 방송이 곧 시작 됩니다.</p>
+							<span><span class="colr_green">라이브</span> 방송 10분 전</span>
+						</a>
+						<span class="time">10분 전</span>
+					</v-list-item>
+				</v-card>
+				
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
 import {Component, Vue, Prop} from 'vue-property-decorator';
+import $ from 'jquery'
 
-@Component({
-    components: {},
+@Component({  
+		components: {},
 })
 export default class Alram extends Vue {
-    @Prop() private src!: string;
-    @Prop() private close!: string;
+	closeModal() {
+		this.$emit('close');
+		$('html, body').removeClass('noscroll')
+	}
 }
-
 </script>
-<style scoped>
-.modal-popup {
-  position: fixed;
-  z-index: 999;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 1);
-  display: table;
-  text-align: center;
-  transition: opacity 0.3s ease;
-  color:#fff;
-}
-.btn-close {
-  color:#fff;
-}
-</style>
--- src/components/component-name/Header.vue
+++ src/components/component-name/Header.vue
@@ -1,36 +1,38 @@
 <template>
-  <div class="header">
+	<div class="header">
 		<h1 class="logo">
 			<span class="a11y">MIZI HOME</span>
 		</h1>
 
-    <div class="util">
-      <v-btn @click="onClickAlram()" class="btn-ic btn_alram new">
-        <i class="new"></i>
-        <span class="a11y">Alram</span>
-      </v-btn>	
-    </div>
-    <template v-if="alramModal">
-      <alram-modal @close="alramModal = false">
-      </alram-modal>
-    </template>
+		<div class="util">
+			<v-btn @click="onClickAlram()" class="btn-ic btn_alram new">
+				<i class="new"></i>
+				<span class="a11y">Alram</span>
+			</v-btn>	
+		</div>
+		<template v-if="alramModal">
+			<alram-modal @close="alramModal = false">
+			</alram-modal>
+		</template>
 	</div>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import AlramModal from '@/components/component-name/Alram.vue'
+import $ from 'jquery'
 
 @Component({
-    components: {
-      AlramModal
-    }
+		components: {
+			AlramModal
+		}
 })
 export default class Header extends Vue{
-  private alramModal = false;
+	private alramModal = false;
 
-  onClickAlram(){
-    this.alramModal=true;
-  }  
+	onClickAlram(){
+		this.alramModal=true;
+		$('html, body').addClass('noscroll')
+	}  
 } 
 </script>
